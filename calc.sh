156a8ab1cab0d451cd1317226b26447f644ac7f8
Jake Hunter <jake7hunter@hmail.com>
2021-11-10 01:21:18 -0500

    added if statement to end script if unknown input is entered

--- calc.sh
+++ calc.sh
@@ -3,12 +3,17 @@ read NAME
 echo "Nice to meet you and welcome to the calculator, $NAME!"
 
 echo "what would you like to do?"
-echo "1. add"
-echo "2. subract"
-echo "3. multiply"
-echo "4. divide"
+echo "1. Add"
+echo "2. Subract"
+echo "3. Multiply"
+echo "4. Divide"
 
 read CHOICE
+
+if [[ $CHOICE -gt 4 ]]; then
+echo "Unknown prompt. Please try again"
+exit
+fi
 
 echo "please enter the first number"
 read N1
